dd2f106ce8694d20f5941aa15e5a9607a062e5b9
nnqq <43930873+nnqq@users.noreply.github.com>
2021-07-17 23:34:23 +0300

    open source fixes

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>
-    <div
-      class="mt-3 p-3 bg-light text-right"
-    >
+  <b-row class="bg-light mt-3 p-3">
+    <b-col class="text-left">
+      <b-link
+        target="_blank"
+        href="https://github.com/leaq-ru"
+      >
+        GitHub
+      </b-link>
+    </b-col>
+    <b-col class="text-right">
       LEAQ / {{ new Date().getFullYear() }}
-    </div>
-  </div>
+    </b-col>
+  </b-row>
 </template>
